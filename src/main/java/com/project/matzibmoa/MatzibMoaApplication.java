c2a92a928f5134ce649531656a83bc3dc86c401f
ybkim-dev <beanybigggy@gmail.com>
2023-02-08 03:34:17 +0900

    feat : 기본 프로젝트 구조 설정

---
+++ src/main/java/com/project/matzibmoa/MatzibMoaApplication.java
@@ -0,0 +1,13 @@
+package com.project.matzibmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatzibMoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatzibMoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/matzibmoa/MatzibMoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.matzibmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatzibMoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
